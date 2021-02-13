97c626c2cbdbe87bfd2d41372bb871ca20921a92
koyawase <waseemkoya@gmail.com>
2021-02-13 18:42:58 +1300

    create get request with path varaible

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RequestMapping("/api/v1/person")
 @RestController
@@ -26,5 +27,13 @@ public class PersonController {
     @GetMapping
     public List<Person> getAllPeople() {
         return personService.getAllPeople();
+    }
+
+//   appending to url path for request
+//   e.g. /api/v1/person/api
+    @GetMapping(path = "{id}")
+//  Grabs id from path and uses that to get person
+    public Person getPersonById(@PathVariable("id") UUID id) {
+        return personService.getPersonById(id).orElse(null);
     }
 }
